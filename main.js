fbf125cb4bb784644a5d6a8179c34a053937e283
Yachont <jakob_r1@gmx.de>
2022-02-11 23:16:35 +0100

    first geogebra test integration (copy paste code)

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-console.log("sup");
-
-// thinking of wether to use onclick button in js or html (https://www.w3schools.com/jsref/event_onclick.asp), but html is easier to see what function is being executed. so this does nothing. remove and optimize.
-// document.getElementById("create-example-function");
-
-function clgHi() {
-  console.log("Hi!");
-}
+var params = {
+  appName: "graphing",
+  width: 800,
+  height: 600,
+  showToolBar: true,
+  showAlgebraInput: true,
+  showMenuBar: true,
+};
+var applet = new GGBApplet(params, true);
+window.addEventListener("load", function () {
+  applet.inject("ggb-element");
+});
