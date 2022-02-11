ccaf0b54b63d4fb478a597aec6d369e002315468
Yachont <jakob_r1@gmx.de>
2022-02-11 23:00:39 +0100

    improved responsive UI layout

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("sup");
+
+// thinking of wether to use onclick button in js or html (https://www.w3schools.com/jsref/event_onclick.asp), but html is easier to see what function is being executed. so this does nothing. remove and optimize.
+// document.getElementById("create-example-function");
+
+function clgHi() {
+  console.log("Hi!");
+}
