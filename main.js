2e2f6f5e5622c822822cd859a81c21668cd454b8
Yachont <jakob_r1@gmx.de>
2022-03-28 21:09:14 +0200

    test button now logs to console on click

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("main.js says hi!");
+
+document.getElementById("create-example-function").addEventListener("click", function(){
+    console.log("the button says hi and sup");
+});
+
+// function clgHi() {
+//     console.log("the button says hi and sup");
+// }
