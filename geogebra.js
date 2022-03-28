d98cbcc2110095c30e2f4a423d59e23fd85a8b78
Yachont <jakob_r1@gmx.de>
2022-03-28 20:44:58 +0200

    geogebra seperate js file

---
+++ geogebra.js
@@ -0,0 +1,14 @@
+var params = {
+  appName: "graphing",
+  width: 1000,
+  height: 800,
+  showToolBar: true,
+  showAlgebraInput: true,
+  showMenuBar: true,
+};
+var applet = new GGBApplet(params, true);
+window.addEventListener("load", function () {
+  applet.inject("ggb-element");
+});
+
+// CHECK GITHUB "ISSUES" FOR IDEAS N STUFF.
